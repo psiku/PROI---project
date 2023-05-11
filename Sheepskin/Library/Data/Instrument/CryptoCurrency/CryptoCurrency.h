74228e9f654a0f3a7662ba83b42aac127a68c8e4
Bartosz <01178579@pw.edu.pl>
2023-05-11 14:58:46 +0200

    added specific instrument class

---
+++ Sheepskin/Library/Data/Instrument/CryptoCurrency/CryptoCurrency.h
@@ -0,0 +1,14 @@
+//
+// Created by barte on 11.05.2023.
+//
+
+#ifndef SHEEPSKIN_CRYPTOCURRENCY_H
+#define SHEEPSKIN_CRYPTOCURRENCY_H
+
+
+class CryptoCurrency {
+
+};
+
+
+#endif //SHEEPSKIN_CRYPTOCURRENCY_H
---
+++ Sheepskin/Library/Data/Instrument/ETF/ETF.h
@@ -0,0 +1,14 @@
+//
+// Created by barte on 11.05.2023.
+//
+
+#ifndef SHEEPSKIN_ETF_H
+#define SHEEPSKIN_ETF_H
+
+
+class ETF {
+
+};
+
+
+#endif //SHEEPSKIN_ETF_H

a962137085bf413051b64edd0935681ae002ec0e
Wiktor Kruszewicz <wiktorkruszewicz@gmail.com>
2023-05-11 16:13:16 +0200

    Added more tests

--- Sheepskin/Tests/StrategyTests.cpp
+++ Sheepskin/Tests/StrategyTests.cpp
@@ -23,3 +23,18 @@ TEST(StrategyTest, SecondStrategyTest) {
     ASSERT_FLOAT_EQ(result.getFallChance(), 0.2);
     ASSERT_FLOAT_EQ(result.getRiseChance(), 0.8);
 }
+TEST(StrategyTest, BothStrategiesTest) {
+    Stock stock("test");
+    FirstStrategy strategy1(stock);
+    SecondStrategy strategy2(stock);
+    std::vector<Strategy*> vec;
+
+    vec.emplace_back(&strategy1);
+    vec.emplace_back(&strategy2);
+
+
+    ASSERT_FLOAT_EQ(vec[0]->eval().getFallChance(), 0.5);
+    ASSERT_FLOAT_EQ(vec[0]->eval().getRiseChance(), 0.5);
+    ASSERT_FLOAT_EQ(vec[1]->eval().getFallChance(), 0.2);
+    ASSERT_FLOAT_EQ(vec[1]->eval().getRiseChance(), 0.8);
+}

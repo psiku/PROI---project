9a3dfddef41dd74d2315af4870b30c35e4ba6377
gamemeine <grzegorz.statkiewicz@gmail.com>
2023-05-31 23:38:34 +0200

    Setup validator

---
+++ Sheepskin/Library/Validator/Validator.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Grzegorz Statkiewicz on 31/05/2023.
+//
+
+#include "Validator.h"
+
+bool Validator::validate(const Instrument& instrument) {
+    return false;
+}
---
+++ Sheepskin/Library/Validator/Validator.h
@@ -0,0 +1,15 @@
+//
+// Created by Grzegorz Statkiewicz on 31/05/2023.
+//
+
+#ifndef SHEEPSKIN_VALIDATOR_H
+#define SHEEPSKIN_VALIDATOR_H
+#include "../Data/Instrument/Instrument/Instrument.h"
+
+class Validator {
+public:
+    static bool validate(const Instrument& instrument);
+};
+
+
+#endif //SHEEPSKIN_VALIDATOR_H

7937c0bb43349bb6a813b28d46921b700df46ed8
gamemeine <grzegorz.statkiewicz@gmail.com>
2023-06-01 01:14:03 +0200

    Remove redefinition

--- Sheepskin/Library/Validator/Validator.cpp
+++ Sheepskin/Library/Validator/Validator.cpp
@@ -50,12 +50,3 @@ bool Validator::validate(Instrument& instrument) {
         && checkDateRepetition(instrument);
 }
 
-bool Validator::checkPrice(Instrument &instrument) {
-    for(auto record : instrument){
-        if(record.open < 0 || record.high < 0 || record.low < 0 || record.close < 0){
-            return false;
-        }
-    }
-    return true;
-}
-

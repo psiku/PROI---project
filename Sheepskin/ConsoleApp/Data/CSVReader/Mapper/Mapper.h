6dd4075e11dd9d9ce9da3d53bd813e31367e35fe
Wiktor Kruszewicz <wiktorkruszewicz@gmail.com>
2023-06-01 01:54:04 +0200

    Implemented mapper mapping the CSV to Instruments

--- Sheepskin/ConsoleApp/Data/CSVReader/Mapper/Mapper.h
+++ Sheepskin/ConsoleApp/Data/CSVReader/Mapper/Mapper.h
@@ -13,7 +13,7 @@ class Mapper {
 public:
 //    Mapper();
     Mapper() = default;
-    std::vector<Instrument*> map(CSVResult);
+    std::vector<Instrument*> mapToInstruments(CSVResult);
     template <class T>
     T translate(std::string val);
 };
@@ -27,6 +27,8 @@ public:
 #include <utility>
 #include <string>
 #include <vector>
+#include "Data/Instrument/ETF/ETF.h"
+#include "Data/Instrument/CryptoCurrency/CryptoCurrency.h"
 //#include "Mapper.h"
 
 template <>
@@ -36,9 +38,14 @@ int Mapper::translate<int>(std::string val) {
 
 template <>
 long double Mapper::translate<long double>(std::string val) {
-    std::cout << val << " ";
     return std::stold(val);
 }
+
+template <>
+double Mapper::translate<double>(std::string val) {
+    return std::stod(val);
+}
+
 template <>
 time_t Mapper::translate<time_t>(std::string val) {
     struct std::tm tm;
@@ -53,10 +60,50 @@ std::string Mapper::translate<std::string>(std::string val) {
     return val;
 }
 
-std::vector<Instrument*> map(CSVResult) {
+std::vector<Instrument*> Mapper::mapToInstruments(CSVResult result) {
     std::map<std::string, Instrument*> instruments;
 
-    
+    while(!result.end()) {
+        auto row = result.getRow();
+
+
+        //name,type,datetime,open,high,low,close,volume
+
+        time_t date = translate<time_t>(row[2]);
+        double open = translate<double>(row[3]);
+        double high = translate<double>(row[4]);
+        double low = translate<double>(row[5]);
+        double close = translate<double>(row[6]);
+        int volume = translate<int>(row[7]);
+
+        Record record(date, open, high, low, close, volume);
+
+        std::string name = translate<std::string>(row[0]);
+
+        if(instruments.find(name) == instruments.end()) {
+            std::string type = translate<std::string>(row[1]);
+
+            if(type == "etf") {
+                ETF etf(name);
+                instruments[name] = &etf;
+            }
+            else if(type == "stock") {
+                Stock stock(name);
+                instruments[name] = &stock;
+            }
+            else if(type == "crypto") {
+                CryptoCurrency crypto(name);
+                instruments[name] = &crypto;
+            }
+        }
+        instruments[name]->addRecord(record);
+    }
+    std::vector<Instrument*> res;
+    for(const auto& [key, value] : instruments) {
+        res.emplace_back(value);
+    }
+
+    return res;
 }
 
 #endif //SHEEPSKIN_MAPPER_H

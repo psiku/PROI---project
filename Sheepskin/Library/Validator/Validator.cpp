dfc6bb89ff61d40500cf6f08bf08691f3141e85e
gamemeine <grzegorz.statkiewicz@gmail.com>
2023-06-01 00:39:58 +0200

    Implement dates validation

--- Sheepskin/Library/Validator/Validator.cpp
+++ Sheepskin/Library/Validator/Validator.cpp
@@ -4,19 +4,25 @@
 
 #include "Validator.h"
 
-bool Validator::checkBorderPrices(const Instrument &instrument) {
+bool Validator::checkBorderPrices(Instrument &instrument) {
     return false;
 }
 
-bool Validator::checkPrice(const Instrument &instrument) {
+bool Validator::checkPrice(Instrument &instrument) {
     return false;
 }
 
-bool Validator::checkDateRepetition(const Instrument &instrument) {
-    return false;
+bool Validator::checkDateRepetition(Instrument &instrument) {
+    std::vector<time_t> dates;
+    for(auto record : instrument) {
+        dates.push_back(record.date);
+    }
+
+    std::sort(dates.begin(), dates.end());
+    return std::adjacent_find(dates.begin(), dates.end()) == dates.end();
 }
 
-bool Validator::validate(const Instrument& instrument) {
+bool Validator::validate(Instrument& instrument) {
     return checkPrice(instrument)
         && checkBorderPrices(instrument)
         && checkDateRepetition(instrument);

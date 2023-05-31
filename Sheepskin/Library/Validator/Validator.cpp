5c91c9b36a54c10ea27a21fc536db6ec664cdebb
gamemeine <grzegorz.statkiewicz@gmail.com>
2023-06-01 00:55:45 +0200

    Implement check order

--- Sheepskin/Library/Validator/Validator.cpp
+++ Sheepskin/Library/Validator/Validator.cpp
@@ -4,6 +4,14 @@
 
 #include "Validator.h"
 
+bool Validator::checkPrice(Instrument &instrument) {
+    for(auto record : instrument){
+        if(record.open >= 0 && record.high >= 0 && record.low >= 0 && record.close >= 0){
+            return true;
+        }
+    }
+    return false;
+}
 
 bool Validator::checkBorderPrices(Instrument &instrument) {
     for(auto record : instrument){
@@ -27,18 +35,18 @@ bool Validator::checkDateRepetition(Instrument &instrument) {
     return std::adjacent_find(dates.begin(), dates.end()) == dates.end();
 }
 
+bool Validator::checkDateOrder(Instrument &instrument) {
+    std::vector<time_t> dates;
+    for(auto record : instrument) {
+        dates.push_back(record.date);
+    }
+
+    return std::is_sorted(dates.begin(), dates.end());
+}
+
 bool Validator::validate(Instrument& instrument) {
     return checkPrice(instrument)
         && checkBorderPrices(instrument)
         && checkDateRepetition(instrument);
-}
-
-bool Validator::checkPrice(Instrument &instrument) {
-    for(auto record : instrument){
-        if(record.open >= 0 && record.high >= 0 && record.low >= 0 && record.close >= 0){
-            return true;
-        }
-    }
-    return false;
 }
 

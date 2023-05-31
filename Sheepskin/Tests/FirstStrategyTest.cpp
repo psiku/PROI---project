a921d6c61ae5065a4207415e9c84c016634e0e5a
Bartosz <01178579@pw.edu.pl>
2023-05-31 09:33:19 +0200

    Fixed tests for sumOfDifference()

--- Sheepskin/Tests/FirstStrategyTest.cpp
+++ Sheepskin/Tests/FirstStrategyTest.cpp
@@ -112,28 +112,22 @@ TEST(FirstStrategyTest, FirstStrategyCalculateSumOfDifference) {
     FirstStrategy strategy(&stock);
     long double result = strategy.sumOfDifference(strategy.listOfTangens());
 
-    ASSERT_FLOAT_EQ(result, 20.0);
+    ASSERT_FLOAT_EQ(result, 120.0);
 }
 TEST(FirstStrategyTest, FirstStrategyCalculateSumOfDifferenceIfThereWasNotChangeInState) {
     Stock stock("id");
     Record record1(1647580800, 2485.00, 2505.00, 2470.00, 2500.00, 2);
-    Record record2(1647580860, 2545.00, 2565.00, 2530.00, 2510.00, 1.5);
-    Record record3(1647580920, 2605.00, 2625.00, 2590.00, 2520.00, 1.0);
-    Record record4(1647580980, 2665.00, 2685.00, 2650.00, 2530.00, 0.5);
-    Record record5(1647581040, 2725.00, 2745.00, 2710.00, 2540.00, 0.0);
-    Record record6(1647581100, 2785.00, 2805.00, 2770.00, 2550.00, -0.5);
-    Record record7(1647581160, 2845.00, 2865.00, 2830.00, 2560.00, -1.0);
+    Record record2(1647580860,  2485.00, 2505.00, 2470.00, 2510.00, 2);
+    Record record3(1647580920,  2485.00, 2507.00, 2470.00, 2520.00, 2);
+    Record record4(1647580980,  2485.00, 2509.00, 2470.00, 2530.00, 2);
     stock.addRecord(record1);
     stock.addRecord(record2);
     stock.addRecord(record3);
     stock.addRecord(record4);
-    stock.addRecord(record5);
-    stock.addRecord(record6);
-    stock.addRecord(record7);
     FirstStrategy strategy(&stock);
     long double result = strategy.sumOfDifference(strategy.listOfTangens());
 
-    ASSERT_FLOAT_EQ(result, 60.0);
+    ASSERT_FLOAT_EQ(result, 4.5);
 }
 TEST(FirstStrategyTest, FirstStrategyEvalResult) {
     Stock stock("id");

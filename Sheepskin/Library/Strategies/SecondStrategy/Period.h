bcccee7aecb31fb4eb21751b2e9ecf0c76a96551
gamemeine <grzegorz.statkiewicz@gmail.com>
2023-06-01 12:59:03 +0200

    Add strategy functions declarations

---
+++ Sheepskin/Library/Strategies/SecondStrategy/Period.h
@@ -0,0 +1,29 @@
+//
+// Created by Grzegorz Statkiewicz on 01/06/2023.
+//
+
+#ifndef SHEEPSKIN_PERIOD_H
+#define SHEEPSKIN_PERIOD_H
+
+#include <vector>
+#include "../../Data/Record/Record.h"
+
+struct Period {
+
+private:
+    std::vector<Record> records;
+
+public:
+    time_t to() const {
+        return std::max_element(records.begin(), records.end(), [&](const Record& a, const Record& b){ return a.date < b.date; })->date;
+    }
+
+    time_t from() const {
+        return std::min_element(records.begin(), records.end(), [&](const Record& a, const Record& b){ return a.date < b.date; })->date;
+    }
+
+    std::vector<Record> getRecords() const { return records; }
+
+};
+
+#endif //SHEEPSKIN_PERIOD_H
--- Sheepskin/Library/Strategies/SecondStrategy/SecondStrategy.h
+++ Sheepskin/Library/Strategies/SecondStrategy/SecondStrategy.h
@@ -7,11 +7,18 @@
 
 
 #include "../Strategy/Strategy.h"
+#include "Stage.h"
+#include "Period.h"
 
 class SecondStrategy: public Strategy  {
 public:
     explicit SecondStrategy(Instrument* instrument);
     Forecast eval() override;
+
+    std::vector<Period> splitByFibonacci(Instrument* instrument);
+    double getPeriodAveragePriceChange(std::vector<Period> periods);
+    Stage calculatePeriodStage(Period period);
+    double getAverageStage(std::vector<Stage> stages);
 };
 
 

8139a071cfef2d9a10e19e623fe79056ae156c32
Wiktor Kruszewicz <wiktorkruszewicz@gmail.com>
2023-06-01 17:51:12 +0200

    Added more tests

--- Sheepskin/Tests/CSVTests.cpp
+++ Sheepskin/Tests/CSVTests.cpp
@@ -7,18 +7,18 @@
 #include "Data/CSVReader/Mapper/Mapper.h"
 #include "Data/CSVReader/CSVReader/CSVReader.h"
 //
-//TEST(CSVTests, MapperStringTest) {
-//    std::vector<std::string> vec;
-//    vec.emplace_back("1");
-//    vec.emplace_back("2");
-//    vec.emplace_back("3");
-//    std::vector<int> expected_ans = {1,2,3};
-////
-//    Mapper mapper;
-//    for(int i = 0 ; i < vec.size() ; i++) {
-//        ASSERT_EQ(expected_ans[i], mapper.translate<int>(vec[i]));
-//    }
-//}
+TEST(CSVTests, MapperStringTest) {
+    std::vector<std::string> vec;
+    vec.emplace_back("1");
+    vec.emplace_back("2");
+    vec.emplace_back("3");
+    std::vector<int> expected_ans = {1,2,3};
+
+    Mapper mapper;
+    for(int i = 0 ; i < vec.size() ; i++) {
+        ASSERT_EQ(expected_ans[i], mapper.translate<int>(vec[i]));
+    }
+}
 TEST(CSVTests, CSVReaderTest) {
    CSVReader reader;
 //    reader.read(file);
@@ -27,8 +27,6 @@ TEST(CSVTests, CSVReaderTest) {
    auto row = res.getRow();
    for(int i = 0 ; i < values.size() ; i++)
        ASSERT_EQ(row[i], values[i]);
-//    reader.read();
-//    /*CSVResult<int, double, std::string> res = */reader.getMapped();
 }
 TEST(CSVTests, CSVReaderToMapperTest) {
     CSVReader reader;
@@ -41,8 +39,4 @@ TEST(CSVTests, CSVReaderToMapperTest) {
     for(auto instrument:*instruments) {
         ASSERT_TRUE(instrument->getRecords().size() > 0);
     }
-//    for(auto *instrument:instruments) {
-//        (instrument->getRecords());
-//    }
-//    ASSERT_TRUE(instruments[0]->getRecords().size() > 0);
 }

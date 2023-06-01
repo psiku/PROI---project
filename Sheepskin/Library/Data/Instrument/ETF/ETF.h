68f6e015d0734a964abc584c8745ab685255d45d
Bartosz <01178579@pw.edu.pl>
2023-06-01 14:01:14 +0200

    Added dosctrings for ETF, Cryptocurrency and Stock

--- Sheepskin/Library/Data/Instrument/ETF/ETF.h
+++ Sheepskin/Library/Data/Instrument/ETF/ETF.h
@@ -8,11 +8,17 @@
 
 #include "../Instrument/Instrument.h"
 
+
+// Class ETF is a class which contains vector of Records and id of ETF
+//it is derived from Instrument
 class ETF: public Instrument {
 public:
     explicit ETF(std::string id);
-    double getVolatility(const Record &record) override;    //
-    double getPrice(const Record &record) override;         // returns average value of 4 prices (high, low, close, open)
+    double getVolatility(const Record &record) override;
+    //getPrice method returns price of the ETF, based on the record average price
+    //@param record - record which will be used to calculate price
+    //@return price of the ETF
+    double getPrice(const Record &record) override;
 };
 
 

5a35da0e26301810859675d9b5885a019d35be58
Bartosz <01178579@pw.edu.pl>
2023-06-01 10:01:09 +0200

    Added ExceptionHandler

---
+++ Sheepskin/Tests/ExceptionHandlerTest.cpp
@@ -0,0 +1,85 @@
+//
+// Created by barte on 01.06.2023.
+//
+#include <gtest/gtest.h>
+#include <exception>
+#include <stdexcept>
+#include "ExceptionHandler/ExceptionHandler.h"
+
+TEST(ExceptionHandlerTest, InvalidFormatExceptionTest) {
+    ExceptionHandler handler;
+    InvalidFormatException ex;
+
+    try {
+    throw ex;
+    } catch (const std::exception& e) {
+    std::stringstream ss;
+    ss << "Handling Exception: " << ex.what() << std::endl;
+    testing::internal::CaptureStdout();
+    handler.handle(e);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, ss.str());
+    }
+}
+
+TEST(ExceptionHandlerTest, NotEnoughDataExceptionTest) {
+    ExceptionHandler handler;
+    NotEnoughDataException ex;
+
+    try {
+    throw ex;
+    } catch (const std::exception& e) {
+    std::stringstream ss;
+    ss << "Handling Exception: " << ex.what() << std::endl;
+    testing::internal::CaptureStdout();
+    handler.handle(e);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, ss.str());
+    }
+}
+TEST(ExceptionHandlerTest, InvalidDataExceptionTest) {
+    ExceptionHandler handler;
+    InvalidDataException ex;
+
+    try {
+        throw ex;
+    } catch (const std::exception& e) {
+        std::stringstream ss;
+        ss << "Handling Exception: " << ex.what() << std::endl;
+        testing::internal::CaptureStdout();
+        handler.handle(e);
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, ss.str());
+    }
+}
+TEST(ExceptionHandlerTest, InvalidInstrumentNameExceptionTest) {
+    ExceptionHandler handler;
+    InvalidInstrumentNameException ex;
+
+    try {
+        throw ex;
+    } catch (const std::exception& e) {
+        std::stringstream ss;
+        ss << "Handling Exception: " << ex.what() << std::endl;
+        testing::internal::CaptureStdout();
+        handler.handle(e);
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, ss.str());
+    }
+}
+
+TEST(ExceptionHandlerTest, UnknownExceptionTest) {
+    ExceptionHandler handler;
+    std::runtime_error ex("Runtime error");
+
+    try {
+    throw ex;
+    } catch (const std::exception& e) {
+    std::stringstream ss;
+    ss << "Handling unknown exception: " << ex.what() << std::endl;
+    testing::internal::CaptureStdout();
+    handler.handle(e);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, ss.str());
+    }
+}

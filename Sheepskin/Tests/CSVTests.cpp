f549142a6a769fa02c915d3b4a879a94a9126a46
Wiktor Kruszewicz <wiktorkruszewicz@gmail.com>
2023-06-01 11:33:50 +0200

    Added more tests for CSVReader

--- Sheepskin/Tests/CSVTests.cpp
+++ Sheepskin/Tests/CSVTests.cpp
@@ -1,28 +1,42 @@
 ////
 //// Created by Wiktor Kruszewicz on 18/05/2023.
 ////
-//#include <gtest/gtest.h>
-//#include <string>
-//#include <vector>
-//#include "Data/CSVReader/Mapper/Mapper.h"
-//#include "Data/CSVReader/CSVReader/CSVReader.h"
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+#include "Data/CSVReader/Mapper/Mapper.h"
+#include "Data/CSVReader/CSVReader/CSVReader.h"
+
+TEST(CSVTests, MapperStringTest) {
+    std::vector<std::string> vec;
+    vec.emplace_back("1");
+    vec.emplace_back("2");
+    vec.emplace_back("3");
+    std::vector<int> expected_ans = {1,2,3};
 //
-//TEST(CSVTests, MapperStringTest) {
-//    std::vector<std::string> vec;
-//    vec.emplace_back("1");
-//    vec.emplace_back("2");
-//    vec.emplace_back("3");
-//    std::vector<int> expected_ans = {1,2,3};
-////
-//    Mapper mapper;
-//    for(int i = 0 ; i < vec.size() ; i++) {
-//        ASSERT_EQ(expected_ans[i], mapper.translate<int>(vec[i]));
-//    }
-//}
-//TEST(CSVTests, CSVReaderTest) {
-//    CSVReader reader;
-////    reader.read(file);
-//    CSVReader::read("./test.csv");
-////    reader.read();
-////    /*CSVResult<int, double, std::string> res = */reader.getMapped();
-//}
+    Mapper mapper;
+    for(int i = 0 ; i < vec.size() ; i++) {
+        ASSERT_EQ(expected_ans[i], mapper.translate<int>(vec[i]));
+    }
+}
+TEST(CSVTests, CSVReaderTest) {
+    CSVReader reader;
+//    reader.read(file);
+    CSVResult res = reader.read("Tests/CSVFiles/test1.csv");
+    std::vector<std::string> values = {"123.321", "1", "test"};
+    auto row = res.getRow();
+    for(int i = 0 ; i < values.size() ; i++)
+        ASSERT_EQ(row[i], values[i]);
+//    reader.read();
+//    /*CSVResult<int, double, std::string> res = */reader.getMapped();
+}
+TEST(CSVTests, CSVReaderToMapperTest) {
+    CSVReader reader;
+    CSVResult res = reader.read("Tests/CSVFiles/test2.csv");
+
+    Mapper mapper;
+    auto instruments = mapper.mapToInstruments(res);
+
+    ASSERT_EQ(instruments.size(), 4);
+
+}
